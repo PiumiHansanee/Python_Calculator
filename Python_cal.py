9f4d802a5a43e63dc9c0d6912f90b0db034c2bc4
piumi274 <bhagyapiumi274@gmail.com>
2025-06-21 14:13:30 +0530

    Basic

--- Python_cal.py
+++ Python_cal.py
@@ -1,6 +1,11 @@
 num1 = 5
-num2 = 100
+num2 = 10
 sum = 0
+min =0
 
-sum = num1 + num2
-print(sum)
+
+sum = num1 + num2 
+min = num2 - num1
+mul = num1 * num2
+Div = num2 // num1
+print(sum, min, mul,Div)
